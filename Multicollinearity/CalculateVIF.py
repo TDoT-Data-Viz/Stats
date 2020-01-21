12165e833b7ff3e96831c2782fd1fd18dfd63c0c
Jared Cleghorn <jaredcleghorn@gmail.com>
2020-01-21 10:36:55 -0600

    Added docstring, formed a function, eliminated code.

--- Multicollinearity/CalculateVIF.py
+++ Multicollinearity/CalculateVIF.py
@@ -3,21 +3,12 @@ import scipy as sp
 import pandas as pd
 
 # Read csv as pandas dataframe
-data = pd.read_csv(r"path\table.csv", header=0)
-
-# Convert dataframe columns to lists
-var1 = list(data.VAR1) # VAR1 in this case would be the heading for the column
-var2 = list(data.VAR2)
-
-# Add new variables for each column to be compared. columns with identical numbers, or numbers that are 
-# multiples of another column (e.g. VAR2 = VAR1 * 2) will return a 'singular matrix' error.
-
-
-# Calculate VIF from lists
-ck = np.column_stack([var1, var2])
-cc = sp.corrcoef(ck, rowvar=False)
-vif = np.linalg.inv(cc)
-v = vif.diagonal()
-print(v)
-
-
+def generate_vif(csv):
+    '''Prints the Variance Inflation Factor (VIF) for the values in a given csv.'''
+    # Create pandas dataframe from csv.
+    data = pd.read_csv(csv, header=0)
+    # Calculate VIF from dataframe.
+    cc = sp.corrcoef(data.values, rowvar=False)
+    vif = np.linalg.inv(cc)
+    v = vif.diagonal()
+    print(v)
